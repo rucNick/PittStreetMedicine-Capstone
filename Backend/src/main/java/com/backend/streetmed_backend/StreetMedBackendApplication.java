ce810342d405fa70e4fbbb164735b1afae03c90c
Nick Cao <99495368+rucNick@users.noreply.github.com>
2025-01-31 14:42:41 -0500

    Add files via upload
    Springboot backend

---
+++ Backend/src/main/java/com/backend/streetmed_backend/StreetMedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.streetmed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetMedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetMedBackendApplication.class, args);
+    }
+
+}
