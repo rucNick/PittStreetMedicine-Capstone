aaf2b4f2975e36cbc6e4f9aae9478db7b4f325fe
rucNick <ruc36@pitt.edu>
2025-02-21 21:08:12 -0500

    config the mongodb and delete the target file

---
+++ src/Backend/src/main/java/com/backend/streetmed_backend/repository/CargoImageRepository.java
@@ -0,0 +1,10 @@
+package com.backend.streetmed_backend.repository;
+
+import com.backend.streetmed_backend.document.CargoImage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface CargoImageRepository extends MongoRepository<CargoImage, String> {
+    Optional<CargoImage> findByCargoItemId(Integer cargoItemId);
+    void deleteByCargoItemId(Integer cargoItemId);
+}
---
+++ src/Backend/src/main/java/com/backend/streetmed_backend/service/CargoImageService.java
@@ -0,0 +1,40 @@
+package com.backend.streetmed_backend.service;
+
+import com.backend.streetmed_backend.document.CargoImage;
+import com.backend.streetmed_backend.repository.CargoImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Service
+public class CargoImageService {
+    private final CargoImageRepository imageRepository;
+
+    @Autowired
+    public CargoImageService(CargoImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    public CargoImage storeImage(MultipartFile file, Integer cargoItemId) throws IOException {
+        CargoImage image = new CargoImage();
+        image.setFilename(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setData(file.getBytes());
+        image.setSize(file.getSize());
+        image.setUploadDate(LocalDateTime.now());
+        image.setCargoItemId(cargoItemId);
+
+        return imageRepository.save(image);
+    }
+
+    public CargoImage getImage(Integer cargoItemId) {
+        return imageRepository.findByCargoItemId(cargoItemId)
+                .orElseThrow(() -> new RuntimeException("Image not found for cargo item: " + cargoItemId));
+    }
+
+    public void deleteImage(Integer cargoItemId) {
+        imageRepository.deleteByCargoItemId(cargoItemId);
+    }
+}
