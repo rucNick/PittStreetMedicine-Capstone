f4516c817acc44ead8141257aa83fc45fb5bf8d8
rucNick <ruc36@pitt.edu>
2025-04-03 23:32:03 -0400

    disable email service

--- src/Backend/src/main/java/com/backend/streetmed_backend/service/EmailService.java
+++ src/Backend/src/main/java/com/backend/streetmed_backend/service/EmailService.java
@@ -2,7 +2,6 @@ package com.backend.streetmed_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -24,7 +23,7 @@ public class EmailService {
     private final Executor emailExecutor;
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy 'at' h:mm a");
 
-    private boolean emailServiceEnabled = true;
+    private boolean emailServiceEnabled = false;
 
     // Store OTP codes with expiration timestamps
     private final Map<String, OtpData> otpMap = new HashMap<>();
